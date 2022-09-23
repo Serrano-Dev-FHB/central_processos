aba982351ab8b59ed9a3c167f93d4d2b345855e0
Felipe Henrique <sistemas3@leiloesjudiciais.com.br>
2022-09-23 07:49:34 -0300

    correcao autentication

--- src/services/autenticationApi.ts
+++ src/services/autenticationApi.ts
@@ -3,10 +3,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
 
 export default async function autenticationApi(req: NextApiRequest, res: NextApiResponse){
-    const secret = process.env.secret;
-    const token  = req.headers['authorization'].split(' ')[1];
+    var data: Object = { secret: "", token: "" };
+    data['secret'] = process.env.secret;
+   
+    if(req.headers['authorization']){
+       data['token'] = req.headers['authorization'].split(' ')[1];
+    }
 
-    jwt.verify(token, secret, function(err, decoded){
+    jwt.verify(data['token'], data['secret'], function(err, decoded){
         if (err) return res.status(500).json({ status: false, message: 'Failed to authenticate token.' });
         req['usuario'] = decoded['usuario'];
     });
